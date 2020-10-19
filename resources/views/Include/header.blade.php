0291b174463f2059d887513b6a0a1c1cf52ce740
Ansell Benedy <ansell08.es@gmail.com>
2020-10-19 22:28:12 +0700

    view index

---
+++ resources/views/Include/header.blade.php
@@ -0,0 +1,72 @@
+@section('header')
+<header>
+    <link rel="stylesheet" href="hus/css/bootstrap.min.css">
+    <link rel="stylesheet" href="hus/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="hus/css/magnific-popup.css">
+    <link rel="stylesheet" href="hus/css/font-awesome.min.css">
+    <link rel="stylesheet" href="hus/css/themify-icons.css">
+    <link rel="stylesheet" href="hus/css/nice-select.css">
+    <link rel="stylesheet" href="hus/css/flaticon.css">
+    <link rel="stylesheet" href="hus/css/gijgo.css">
+    <link rel="stylesheet" href="hus/css/animate.css">
+    <link rel="stylesheet" href="hus/css/slick.css">
+    <link rel="stylesheet" href="hus/css/slicknav.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
+
+    <link rel="stylesheet" href="hus/css/style.css">
+    <div class="header-area ">
+        <div id="sticky-header" class="main-header-area">
+            <div class="container-fluid p-0">
+                <div class="row align-items-center no-gutters">
+                    <div class="col-xl-2 col-lg-2">
+                        <div class="logo">
+                            <a href="index.html">
+                                <img src="hus/img/logo.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6">
+                        <div class="main-menu  d-none d-lg-block">
+                            <nav>
+                                <ul id="navigation">
+                                    <li><a class="active" href="index.html">home</a></li>
+                                    <li><a href="about.html">About</a></li>
+                                    <li><a href="#">pages</a>
+                                        <ul class="submenu">
+                                                <li><a href="facilites.html">Facilities</a></li>
+                                                <li><a href="property.html">Properties</a></li>
+                                                <li><a href="elements.html">elements</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">blog</a>
+                                        <ul class="submenu">
+                                            <li><a href="blog.html">blog</a></li>
+                                            <li><a href="single-blog.html">single-blog</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 d-none d-lg-block">
+                        <div class="social_wrap d-flex align-items-center justify-content-end">
+                            <div class="login_text">
+                                <a href="#">Login</a>
+                            </div>
+                            <div class="number">
+                                <p>Call us <a href="#">+001 325 589</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="mobile_menu d-block d-lg-none"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</header>
+@endsection
