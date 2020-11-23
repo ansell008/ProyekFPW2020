934a610b590f041c7d43603c5ce82b4df13c54f6
jennychndr <j.chdr61@gmail.com>
2020-11-23 14:22:26 +0700

    Update detail.blade.php

--- resources/views/Customer/detail.blade.php
+++ resources/views/Customer/detail.blade.php
@@ -1,9 +1,8 @@
-@extends('Include.layout')
-@extends('Include.header2')
-@extends('Include.footer')
+
 
 
 @section('content')
+<button><a href="/homecustomer">Back</a></button>
 <h1>{{$dipilih->apartment_nama}}</h1>
 <img src="/hus/img/banana.jpg" alt="" style="width: 500px;height:400px">
 <h3>{{$dipilih->user_nama}}</h3>
